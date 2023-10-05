76ba7344ffd0fbf9521ef96c40b0f965f3adb24d
KirillSpitsyn <zima197211@gmail.com>
2023-10-04 23:09:02 -0400

    Update App.js

--- Lab1-my-first-react-app/src/App.js
+++ Lab1-my-first-react-app/src/App.js
@@ -4,7 +4,7 @@ import Greeter from './Greeter'; //import the Greeter component
 function App() {
   return (
     <div>
-      <h1>Welcome to My React App</h1>
+      <h1>Welcome to My React App!</h1>
       <Greeter/> 
     </div>
   );
